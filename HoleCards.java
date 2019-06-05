3d03d1e90c927c10418754a4b66799e1afc55fac
иван иванович <ivanivanovich@MacBook-Air-ivan.local>
2019-06-05 15:52:46 -0700

    finishing rename of Hand to HoleCards, improving static/non-static characteristics of Deck, new class for testing Texas Holdem

--- HoleCards.java
+++ HoleCards.java
@@ -1,26 +1,32 @@
 import java.util.ArrayList;
 
 public class HoleCards {
-	ArrayList<Card> hand = new ArrayList<Card>();
+	ArrayList<Card> holeCards = new ArrayList<Card>();
 	
 	public HoleCards(int handSize) {
 		for (int i = 0; i < handSize; i++) {
-			Card cardToAdd = Deck.random();
-			hand.add(cardToAdd);
+			holeCards.add(Deck.random());
+		}
+	}
+	
+	public HoleCards(Deck deck, int handSize) {
+		for (int i = 0; i < handSize; i++ ) {
+			holeCards.add(deck.takeCard());
+			
 		}
 	}
 	
 	public HoleCards(int handSize, boolean isReal) {
 		for (int i = 0; i < handSize; i++) {
 			Card cardToAdd = new Card(Rank.of(3), Suit.CLUBS);
-			hand.add(cardToAdd);
+			holeCards.add(cardToAdd);
 		}
 	}
 	
 	public String toString() {
 		String handString = "";
 		
-		for (Card cardToPrint : hand) {
+		for (Card cardToPrint : holeCards) {
 			handString += cardToPrint.toString() + "\n";
 		}
 		
--- TexasHoldem.java
+++ TexasHoldem.java
@@ -1,4 +1,10 @@
 
 public class TexasHoldem {
-	
+	public static void main (String[] args) {
+		System.out.println("Testing Texas Holdem");
+		
+		Deck deck = new Deck();
+		HoleCards holeCards1 = new HoleCards(2);
+		System.out.println(holeCards1);
+	}
 }
