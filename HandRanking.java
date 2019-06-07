c2a8a239a2e2117a5f29effef58e1bb4fffb6d26
иван иванович <ivanivanovich@MacBook-Air-ivan.local>
2019-06-06 18:54:25 -0700

    modifying hand ranking code for a new method to just check 5 cards for ranking

--- HandRanking.java
+++ HandRanking.java
@@ -3,7 +3,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
-
 public class HandRanking {
 	public static int rank(HoleCards holeCards, Board board, GameType gameType) {
 		if (gameType == GameType.TEXAS_HOLDELM) {
@@ -11,6 +10,77 @@ public class HandRanking {
 		}
 		
 		return 0;
+	}
+	
+	public static int rankFiveCards(ArrayList<Card> cards) { // TODO handle Ace on both sides for straights
+		HashMap<Rank, Integer> ranksMap = new HashMap<Rank, Integer>();
+		for (Card card : cards) {
+			if (ranksMap.containsKey(card.rank)) {
+				ranksMap.replace(card.rank, ranksMap.get(card.rank)+ 1);
+			} else {
+				ranksMap.put(card.rank, 1);
+			}
+		}
+		
+		HashMap<Integer, LinkedList<Rank>> frequencyToRanks = new HashMap<Integer, LinkedList<Rank>>();
+		for(Map.Entry<Rank, Integer> entry : ranksMap.entrySet()){
+			if (frequencyToRanks.containsKey(entry.getValue())) {
+				frequencyToRanks.get(entry.getValue()).add(entry.getKey());
+			} else {
+				LinkedList<Rank> list = new LinkedList<Rank>();
+				list.add(entry.getKey());
+				frequencyToRanks.put(entry.getValue(), list);
+			}
+		}
+		
+		cards.sort(Card.rankComparator);
+		cards.sort(Card.suitComparator);
+		if (cards.get(0).suit == cards.get(4).suit) {
+			if (cards.get(0).rank.value + 1 == cards.get(1).rank.value && cards.get(1).rank.value + 1 == cards.get(2).rank.value && cards.get(2).rank.value + 1 == cards.get(3).rank.value && cards.get(3).rank.value + 1 == cards.get(4).rank.value) {
+				System.out.println("STRAIGHT FLUSH");
+				return 9;
+			}
+		}
+		
+		if (frequencyToRanks.containsKey(4)) {
+			System.out.println("FOUR OF A KIND");
+			return 8;
+		}
+		
+		if (frequencyToRanks.containsKey(3) && frequencyToRanks.containsKey(2)) {
+			System.out.println("FULL HOUSE");
+			return 7;
+		}
+		
+		if (cards.get(0).suit == cards.get(4).suit) {
+			System.out.println("FLUSH");
+			return 6;
+		}
+		
+		cards.sort(Card.rankComparator);
+		System.out.println(cards);
+		if (cards.get(0).rank.value + 1 == cards.get(1).rank.value && cards.get(1).rank.value + 1 == cards.get(2).rank.value && cards.get(2).rank.value + 1 == cards.get(3).rank.value && cards.get(3).rank.value + 1 == cards.get(4).rank.value) {
+			System.out.println("STRAIGHT");
+			return 5;
+		}
+		
+		if (frequencyToRanks.containsKey(3)) {
+			System.out.println("THREE OF A KIND");
+			return 4;
+		}
+		
+		if (frequencyToRanks.containsKey(2) && frequencyToRanks.get(2).size() > 1) {
+			System.out.println("TWO PAIR");
+			return 3;
+		} 
+		
+		if (frequencyToRanks.containsKey(2)) {
+			System.out.println("PAIR");
+			return 2;
+		}
+		
+		System.out.println("HIGH CARD");
+		return 1;
 	}
 	
 	public static int rankTexasHoldem(HoleCards holeCards, Board board) {
@@ -21,8 +91,6 @@ public class HandRanking {
 		for (Card card : board.board) {
 			cards.add(card);
 		}
-		cards.sort(Card.rankComparator);
-		cards.sort(Card.suitComparator);
 		
 		HashMap<Rank, Integer> ranksMap = new HashMap<Rank, Integer>();
 		for (Card card : cards) {
@@ -44,30 +112,59 @@ public class HandRanking {
 			}
 		}
 		
-		if (cards.get(0).suit == cards.get(4).suit || cards.get(1).suit == cards.get(5).suit || cards.get(2).suit == cards.get(6).suit) {
-			System.out.println("STRAIGHT FLUSH");
-			return 9;
-		} else if (frequencyToRanks.containsKey(4)) {
+		cards.sort(Card.rankComparator);
+		cards.sort(Card.suitComparator);
+		for (int offset = 0; offset <= 2; offset++) {
+			if (cards.get(offset).suit == cards.get(offset + 4).suit) {
+				if (cards.get(offset).rank.value + 1 == cards.get(offset + 1).rank.value && cards.get(offset + 1).rank.value + 1 == cards.get(offset + 2).rank.value && cards.get(offset + 2).rank.value + 1 == cards.get(offset + 3).rank.value && cards.get(offset + 3).rank.value + 1 == cards.get(offset + 4).rank.value) {
+					System.out.println("STRAIGHT FLUSH");
+					return 9;
+				}
+			}
+		}
+		
+		if (frequencyToRanks.containsKey(4)) {
 			System.out.println("FOUR OF A KIND");
 			return 8;
-		} else if (frequencyToRanks.containsKey(3) && frequencyToRanks.containsKey(2)) {
+		}
+		
+		if (frequencyToRanks.containsKey(3) && frequencyToRanks.containsKey(2)) {
 			System.out.println("FULL HOUSE");
 			return 7;
-		} if (cards.get(0).suit == cards.get(4).suit || cards.get(1).suit == cards.get(5).suit || cards.get(2).suit == cards.get(6).suit) {
-			System.out.println("FLUSH");
-			return 6;
-		} else if (frequencyToRanks.containsKey(3)) {
+		}
+		
+		for (int offset = 0; offset <= 2; offset++) {
+			if (cards.get(offset).suit == cards.get(offset + 4).suit) {
+				System.out.println("FLUSH");
+				return 6;
+			}
+		}
+		
+		cards.sort(Card.rankComparator); // TODO not complete
+		System.out.println(cards);
+		for (int offset = 0; offset <= 2; offset++) {
+			if (cards.get(offset).rank.value == 1) {
+				System.out.println("STRAIGHT");
+				return 5;
+			}
+		}
+		
+		if (frequencyToRanks.containsKey(3)) {
 			System.out.println("THREE OF A KIND");
 			return 4;
-		} else if (frequencyToRanks.containsKey(2) && frequencyToRanks.get(2).size() > 1) {
+		}
+		
+		if (frequencyToRanks.containsKey(2) && frequencyToRanks.get(2).size() > 1) {
 			System.out.println("TWO PAIR");
 			return 3;
-		} else if (frequencyToRanks.containsKey(2)) {
+		} 
+		
+		if (frequencyToRanks.containsKey(2)) {
 			System.out.println("PAIR");
 			return 2;
-		} else {
-			System.out.println("HIGH CARD");
-			return 1;
 		}
+		
+		System.out.println("HIGH CARD");
+		return 1;
 	}
 }
