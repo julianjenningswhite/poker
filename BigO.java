3b38e5208ea21640a3e6488e11b1566ed27621fc
иван иванович <ivanivanovich@MacBook-Air-ivan.local>
2019-06-09 05:36:29 -0700

    making the number of hands evaluated in BigO simulations more easily configurable

--- BigO.java
+++ BigO.java
@@ -32,46 +32,42 @@ public class BigO {
 		return ranking;
 	}
 	
-	public static double highWinner(Hand hand1, Hand hand2, Hand hand3, Hand hand4, Hand hand5, Hand hand6, Hand hand7, Hand hand8, Board board) {
-		double[] highScores = new double[9];
-		highScores[0] = bestHighHand(hand1, board);
-		highScores[1] = bestHighHand(hand2, board);
-		highScores[2] = bestHighHand(hand3, board);
-		highScores[3] = bestHighHand(hand4, board);
-		highScores[4] = bestHighHand(hand5, board);
-		highScores[5] = bestHighHand(hand6, board);
-		highScores[6] = bestHighHand(hand7, board);
-		highScores[7] = bestHighHand(hand8, board);
-		double max = 0;
+	public static double highWinner(ArrayList<Hand> hands, Board board) {
+		ArrayList<Double> highScores = new ArrayList<Double>();
+		for (Hand hand : hands) {
+			highScores.add(bestHighHand(hand, board));
+		}
+		
+		double highestScore = 0;
 		int winningHand = 0;
-		for (int i = 0; i < 8; i++) {
-//			System.out.println("Hand no. " + (i + 1) + " has a score of: " + highScores[i]);
-			if (max < highScores[i]) {
-				max = highScores[i];
+		int i = 0;
+		for (Double highScore : highScores) {
+			// System.out.println("Hand no. " + (i + 1) + " has a score of: " + highScore);
+			if (highestScore < highScore) {
+				highestScore = highScore;
 				winningHand = i + 1;
 			}
+			i++;
 		}
-//		return "The high winner is hand no. " + winningHand + ", with a score of: " + max;
-		return max;
+		// System.out.println("The high winner is hand no. " + winningHand + ", with a score of: " + highestScore);
+		
+		return highestScore;
 	}
 	
 	public static int analysis() {
+		int numberOfHands = 8;
 		Deck deck = new Deck();
-		Hand hand1 = new Hand(deck,5);
-		Hand hand2 = new Hand(deck,5);
-		Hand hand3 = new Hand(deck,5);
-		Hand hand4 = new Hand(deck,5);
-		Hand hand5 = new Hand(deck,5);
-		Hand hand6 = new Hand(deck,5);
-		Hand hand7 = new Hand(deck,5);
-		Hand hand8 = new Hand(deck,5);
+		ArrayList<Hand> hands = new ArrayList<Hand>();
+		for (int i = 0; i < numberOfHands; i++) {
+			hands.add(new Hand(deck, 5));
+		}
 		Board board = new Board(deck);
-		return (int) highWinner(hand1, hand2, hand3, hand4, hand5, hand6, hand7, hand8, board);
+		return (int) highWinner(hands, board);
 	}
 	
 	public static void main(String[] args) {
 		HashMap<Integer, Integer> frequency = new HashMap<Integer, Integer>();
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 1000; i++) {
 			int result = analysis();
 			if (frequency.containsKey(result))
 				frequency.put(result, frequency.get(result) + 1);
